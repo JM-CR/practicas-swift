89093a550c465f9a28f903c1382eef1587263708
Josue Mosh <msi_98_98@yahoo.com.mx>
2019-10-10 16:26:55 -0500

    [P1] Esqueleto de la practica

---
+++ P1/SquashGame/SquashGame/Ayuda/Modelo/Ayuda.swift
@@ -0,0 +1,13 @@
+//
+//  Ayuda.swift
+//  SquashGame
+//
+//  Created by Josue Mosiah Contreras Rocha on 10/10/19.
+//  Copyright © 2019 Josue Mosiah Contreras Rocha. All rights reserved.
+//
+
+import Foundation
+
+class Ayuda {
+    
+}
