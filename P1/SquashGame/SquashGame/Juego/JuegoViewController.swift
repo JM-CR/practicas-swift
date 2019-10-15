cac60fe7656913600a70e42593cdf655d90d417e
Josue Mosh <msi_98_98@yahoo.com.mx>
2019-10-15 14:11:03 -0500

    [P1] Redireccion hacia MarcadorViewController cuando pierde

--- P1/SquashGame/SquashGame/Juego/JuegoViewController.swift
+++ P1/SquashGame/SquashGame/Juego/JuegoViewController.swift
@@ -135,25 +135,22 @@ class JuegoViewController: UIViewController, UICollisionBehaviorDelegate {
                 preferredStyle: .alert
             )
             
-            let haciaMarcadores = UIAlertAction(title: "Ir a marcadores.", style: .default) { action in
-                // TODO: Agregar navegación hacia marcadores.
-            }
-            
+            let haciaMarcadores = UIAlertAction(title: "Ir a marcadores.", style: .default, handler: irAMarcadores)
             alerta.addAction(haciaMarcadores)
             self.present(alerta, animated: true, completion: nil)
         }
     }
     
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /**
+     Redirecciona al view controller de marcadores desde el modal.
+     */
+    func irAMarcadores(accion: UIAlertAction) {
+        let inicioVC = self.presentingViewController as! InicioViewController
+        dismiss(animated: true, completion: nil)
+        inicioVC.performSegue(withIdentifier: "marcadores", sender: inicioVC)
+        
+//        let marcadorVC = self.storyboard?.instantiateViewController(withIdentifier: "marcador")
+//        self.present(marcadorVC!, animated: true, completion: nil)
     }
-    */
 
 }
