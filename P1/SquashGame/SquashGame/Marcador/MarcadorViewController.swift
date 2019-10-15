a8b125e30835f204a9e3df31689d7297180a724c
Josue Mosh <msi_98_98@yahoo.com.mx>
2019-10-15 17:20:28 -0500

    [P1] Persistencia del marcador

--- P1/SquashGame/SquashGame/Marcador/MarcadorViewController.swift
+++ P1/SquashGame/SquashGame/Marcador/MarcadorViewController.swift
@@ -10,8 +10,27 @@ import UIKit
 
 class MarcadorViewController: UIViewController {
 
+    @IBOutlet weak var botonRegresar: UIButton!
+    @IBOutlet weak var botonBorrar: UIButton!
+    
+    // Rastreo de mejores puntuaciones
+    var puntuaciones: [Int]? = nil
+    var fechasDeCreacion: [String]? = nil
+    
+    /**
+     Realiza acciones después de que cargó el controlador.
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    /**
+     Realiza acciones antes de que aparezca el controlador.
+     */
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.botonRegresar.layer.cornerRadius = 15
+        self.botonBorrar.layer.cornerRadius = 15
     }
     
     /**
@@ -21,14 +40,31 @@ class MarcadorViewController: UIViewController {
         let inicioVC = self.storyboard?.instantiateViewController(withIdentifier: "inicio")
         self.present(inicioVC!, animated: true, completion: nil)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    /**
+     Borra el marcador del dispositivo y de la pantalla.
+     */
+    @IBAction func borrarMarcador(_ sender: UIButton) {
+        self.puntuaciones?.removeAll()
+        self.fechasDeCreacion?.removeAll()
+        self.guardarHistorial()
     }
-    */
-
+    
+    /**
+     Guarda el historial de puntuaciones en el dispositivo.
+     */
+    private func guardarHistorial() {
+        let defaults = UserDefaults.standard
+        defaults.set(self.fechasDeCreacion, forKey: "fechas")
+        defaults.set(self.puntuaciones, forKey: "puntos")
+    }
+    
+    /**
+     Lee del dispositivo el historial de puntuaciones.
+     */
+    private func cargarHistorial() {
+        let defaults = UserDefaults.standard
+        self.fechasDeCreacion = defaults.stringArray(forKey: "fechas") ?? [String]()
+        self.puntuaciones = defaults.array(forKey: "puntos") as? [Int] ?? [Int]()
+    }
 }
